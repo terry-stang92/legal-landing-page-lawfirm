fdfc3b1c39959c5d82f7be75591a3ab604b76dfc
Fernando Terrazas <portonesaluminorb@gmail.com>
2026-04-27 06:05:00 -0500

    first commit - legal landing page simulation

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('fomr').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('!Gracias! Nos pondremos en contacto contigo pronto');
+    this.rest();
+})
